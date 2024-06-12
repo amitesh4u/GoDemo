2b0dd9c34c4d979b1c08d0258997d5f83bdaacf4
amitesh4u <amitesh4ever@gmail.com>
2024-06-13 01:25:40 +0530

    Implemented newDeck function

--- deck.go
+++ deck.go
@@ -7,14 +7,26 @@ type deck []string
 
 // (d deck) is receiver. Any var of type Deck get access to method Print
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
 
 // Returns a new Deck. No receiver required here
 func newDeck() deck {
-	return deck{}
+	cardSuites := []string{"Heart", "Spades", "Club", "Diamond"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+		"Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	cards := deck{}
+	// blank identifier _, indicates that we want to ignore it.
+	// Else it will create Unused variable error
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, suite+" of "+value)
+		}
+	}
+	return cards
 }
 
 // Shuffle the Deck
