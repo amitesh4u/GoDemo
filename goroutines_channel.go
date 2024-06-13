c22e625e3cc460559c7403c7342c538aa472ccb6
amitesh4u <amitesh4ever@gmail.com>
2024-06-13 23:45:25 +0530

    Implemented go routine flow by making main thread sleep

---
+++ goroutines_channel.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
